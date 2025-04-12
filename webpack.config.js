f0c57ef1c029e73d62868d3dfecfe48fbef1c2c2
timjackleus <timjackleus@gmail.com>
2025-04-12 18:34:51 +0200

    chore: migrate from npm to pnpm and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const path = require( 'path' );
+const path = require("path");
 module.exports = {
-	mode: 'production', // production
-	entry: {
-		'ledyer-payments-checkout': './assets/js/ledyer-payments-checkout',
-		'ledyer-payments-admin': './assets/js/ledyer-payments-admin',
-	},
+  mode: "production", // production
+  entry: {
+    "ledyer-payments": "./src/assets/js/ledyer-payments",
+  },
 
-	output: {
-		filename: '[name].min.js',
-		path: path.resolve( __dirname, './assets/js' ),
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [ '@babel/preset-env' ],
-						plugins: [ '@babel/plugin-proposal-object-rest-spread' ],
-					},
-				},
-			},
-		],
-	},
+  output: {
+    filename: "[name].min.js",
+    path: path.resolve(__dirname, "./assets/js"),
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
 };
